733e5901ec376b06de34ebffc8ccfa417f7ae525
Zoe Ames <zoeames@gmail.com>
2018-11-13 18:30:57 -0600

    setup complete

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+import 'bootstrap';
+
+import './index.scss';
+
+const initializeApp = () => {
+  $('body').html('<h2>Mmmmmmm turkey</h2>');
+  console.log('nap time');
+};
+
+initializeApp();
