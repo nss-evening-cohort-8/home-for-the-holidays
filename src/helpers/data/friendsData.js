13295d81c13d8a157cb0dd50939e52f9a8292236
Callan Morrison <callan.l.morrison@gmail.com>
2018-11-20 19:10:58 -0600

    wip

--- src/helpers/data/friendsData.js
+++ src/helpers/data/friendsData.js
@@ -33,9 +33,7 @@ const getSingleFriend = friendId => new Promise((resolve, reject) => {
     });
 });
 
-const deleteFriend = (friendId) => {
-  console.log(friendId);
-};
+const deleteFriend = friendId => axios.delete(`${firebaseUrl}/friends/${friendId}.json`);
 
 export default {
   getAllFriends,
